d087e62d13b1d7cdb51075f46498170457655b37
Le127 <nereocarbonara@gmail.com>
2021-10-27 18:26:40 -0300

    Card

--- lib/widget/product_card.dart
+++ lib/widget/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_stars/flutter_rating_stars.dart';
 
 class ProductCard extends StatefulWidget {
   const ProductCard({Key? key}) : super(key: key);
@@ -11,15 +12,17 @@ class _ProductCardState extends State<ProductCard> {
 //primera de la lista
   String urlImage =
       "https://http2.mlstatic.com/D_NQ_NP_612012-MLA47560182865_092021-O.webp";
+  double value = 3.5;
+  bool isSelected = false;
+  String dropdownValue = "S";
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
       width: 350,
-      height: 500,
+      height: 550,
       child: Card(
-        color: Colors.red,
         child: Column(
           children: [
             const SizedBox(height: 5),
@@ -65,11 +68,41 @@ class _ProductCardState extends State<ProductCard> {
               ],
             ),
             const SizedBox(height: 10),
-            const Text("Remera Top full"),
+            const Text("Remera Top full",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600)),
             const SizedBox(height: 10),
-            const Text("Price  \$201"),
+            const Text("\$201.99",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
             const SizedBox(height: 10),
-            const Text("Talle : XL"),
+            RatingStars(
+                value: value,
+                valueLabelVisibility: false,
+                onValueChanged: (v) {
+                  setState(() => value = v);
+                }),
+            const SizedBox(height: 10),
+            DropdownButton(
+              items: <String>['S', 'M', 'L', 'XL'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              value: dropdownValue,
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all(const Size(300, 20)),
+                ),
+                onPressed: () {},
+                child: const Text("Pay",
+                    style: TextStyle(fontWeight: FontWeight.bold)))
           ],
         ),
       ),
