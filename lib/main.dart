b0d106ef9d51456d2f25f51c0010504d76f1b022
Le127 <nereocarbonara@gmail.com>
2021-10-28 20:46:38 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 
 import 'package:ecomm_app/pages/unique_product_page.dart';
 import 'package:ecomm_app/pages/all_products_page.dart';
@@ -7,16 +6,6 @@ import 'package:ecomm_app/pages/clothing_page.dart';
 import 'package:ecomm_app/pages/sports_page.dart';
 import 'package:ecomm_app/pages/game_page.dart';
 import 'package:ecomm_app/pages/tech_page.dart';
-=======
-
-import 'package:ecomm_app/pages/accessory_page.dart';
-import 'package:ecomm_app/pages/all_products_page.dart';
-import 'package:ecomm_app/pages/clothing_page.dart';
-import 'package:ecomm_app/pages/game_page.dart';
-import 'package:ecomm_app/pages/tech_page.dart';
-import 'package:ecomm_app/pages/unique_product_page.dart';
-
->>>>>>> 3445dfd2abebd5ce0916dad2f77b42db179fcca6
 
 void main() => runApp(const MyApp());
 
