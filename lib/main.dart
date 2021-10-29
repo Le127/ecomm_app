163ecf465555af0596976a577fefc00468f07aaf
Le127 <nereocarbonara@gmail.com>
2021-10-29 12:15:03 -0300

    firebase fail

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ecomm_app/pages/unique_product_page.dart';
@@ -9,8 +10,67 @@ import 'package:ecomm_app/pages/tech_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String get name => 'ecommflutter';
+
+  FirebaseOptions get firebaseOptions => const FirebaseOptions(
+      apiKey: "AIzaSyAa9fV9zuijX-M9N10Z1kc3tLric9Vxw7M",
+      authDomain: "ecommflutter-f5351.firebaseapp.com",
+      projectId: "ecommflutter-f5351",
+      storageBucket: "ecommflutter-f5351.appspot.com",
+      messagingSenderId: "194768273496",
+      appId: "1:194768273496:web:acbbcafd368190c404ca2d");
+
+  Future<void> initializeDefault() async {
+    FirebaseApp app = await Firebase.initializeApp();
+    // ignore: unnecessary_null_comparison
+    assert(app != null);
+    // ignore: avoid_print
+    print('Initialized default app $app');
+  }
+
+  Future<void> initializeSecondary() async {
+    FirebaseApp app =
+        await Firebase.initializeApp(name: name, options: firebaseOptions);
+
+    // ignore: unnecessary_null_comparison
+    assert(app != null);
+    // ignore: avoid_print
+    print('Initialized $app');
+  }
+
+  void apps() {
+    final List<FirebaseApp> apps = Firebase.apps;
+    // ignore: avoid_print
+    print('Currently initialized apps: $apps');
+  }
+
+  void options() {
+    final FirebaseApp app = Firebase.app(name);
+    final FirebaseOptions options = app.options;
+    // ignore: avoid_print
+    print('Current options for app $name: $options');
+  }
+
+  Future<void> delete() async {
+    final FirebaseApp app = Firebase.app(name);
+    await app.delete();
+    // ignore: avoid_print
+    print('App $name deleted');
+  }
+
+  @override
+  void initState() {
+    initializeDefault();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
