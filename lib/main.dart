9a066618bad45f7dd2be8ed8ccdf30780bf7d156
Le127 <nereocarbonara@gmail.com>
2021-10-29 18:57:54 -0300

    firebase prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import "dart:developer" as developer;
 
 import 'package:ecomm_app/pages/unique_product_page.dart';
 import 'package:ecomm_app/pages/all_products_page.dart';
@@ -8,7 +9,10 @@ import 'package:ecomm_app/pages/sports_page.dart';
 import 'package:ecomm_app/pages/game_page.dart';
 import 'package:ecomm_app/pages/tech_page.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,80 +22,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String get name => 'ecommflutter';
-
-  FirebaseOptions get firebaseOptions => const FirebaseOptions(
-      apiKey: "AIzaSyAa9fV9zuijX-M9N10Z1kc3tLric9Vxw7M",
-      authDomain: "ecommflutter-f5351.firebaseapp.com",
-      projectId: "ecommflutter-f5351",
-      storageBucket: "ecommflutter-f5351.appspot.com",
-      messagingSenderId: "194768273496",
-      appId: "1:194768273496:web:acbbcafd368190c404ca2d");
-
-  Future<void> initializeDefault() async {
-    FirebaseApp app = await Firebase.initializeApp();
-    // ignore: unnecessary_null_comparison
-    assert(app != null);
-    // ignore: avoid_print
-    print('Initialized default app $app');
-  }
-
-  Future<void> initializeSecondary() async {
-    FirebaseApp app =
-        await Firebase.initializeApp(name: name, options: firebaseOptions);
-
-    // ignore: unnecessary_null_comparison
-    assert(app != null);
-    // ignore: avoid_print
-    print('Initialized $app');
-  }
-
-  void apps() {
-    final List<FirebaseApp> apps = Firebase.apps;
-    // ignore: avoid_print
-    print('Currently initialized apps: $apps');
-  }
-
-  void options() {
-    final FirebaseApp app = Firebase.app(name);
-    final FirebaseOptions options = app.options;
-    // ignore: avoid_print
-    print('Current options for app $name: $options');
-  }
-
-  Future<void> delete() async {
-    final FirebaseApp app = Firebase.app(name);
-    await app.delete();
-    // ignore: avoid_print
-    print('App $name deleted');
-  }
-
-  @override
-  void initState() {
-    initializeDefault();
-    super.initState();
-  }
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ecomm App',
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Ecomm App Bar'),
-          ),
-        ),
-      ),
-      initialRoute: "allProducts",
-      routes: {
-        "allProducts": (_) => const AllProductsPage(),
-        "clothing": (_) => const ClothingPage(),
-        "tech": (_) => const TechPage(),
-        "sports": (_) => const SportsPage(),
-        "game": (_) => const GamePage(),
-        "uniqueProduct": (_) => const UniqueProductPage(urlImage: []),
+    return FutureBuilder<FirebaseApp>(
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot<FirebaseApp>? snapshot) {
+        if (snapshot!.hasError) {
+          developer.log("Main , Firebase init. ERROR");
+          return const Text(
+            "Error firebase",
+            textDirection: TextDirection.ltr,
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          developer.log("Main , Firebase init. DONE OK!");
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Ecomm App',
+            home: SafeArea(
+              child: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Ecomm App Bar'),
+                ),
+              ),
+            ),
+            initialRoute: "allProducts",
+            routes: {
+              "allProducts": (_) => const AllProductsPage(),
+              "clothing": (_) => const ClothingPage(),
+              "tech": (_) => const TechPage(),
+              "sports": (_) => const SportsPage(),
+              "game": (_) => const GamePage(),
+              "uniqueProduct": (_) => const UniqueProductPage(urlImage: []),
+            },
+          );
+        }
+        return const CircularProgressIndicator();
       },
     );
   }
