6bd59b450c39237218dd3853e4ae8278202e1af2
Le127 <nereocarbonara@gmail.com>
2021-11-03 10:16:57 -0300

    Read db

--- lib/pages/clothing_page.dart
+++ lib/pages/clothing_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecomm_app/db/helpers/clothing_helpers.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +16,9 @@ class ClothingPage extends StatelessWidget {
     if (size.width >= 1091 && size.height >= 624) {
       widthSize = 400;
     }
+
+    CollectionReference clothings =
+        FirebaseFirestore.instance.collection('clothings');
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: () {
@@ -40,49 +44,74 @@ class ClothingPage extends StatelessWidget {
         ),
         backgroundColor: Colors.yellow.shade300.withOpacity(0.9),
       ),
-      body: Center(
-        child: SizedBox(
-          width: widthSize,
-          // height: heightSize,
-          //constraints: BoxConstraints(minWidth: minWidth, minHeight: minHeight),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: clothings.doc("INLhUzsvzQKQSSKMx5Xf").get(),
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Text("Something went wrong");
+              }
 
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.only(top: 20),
-            physics: const BouncingScrollPhysics(),
-            child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 2,
-                mainAxisSpacing: 2,
-                childAspectRatio: 0.8,
-                shrinkWrap: true,
-                children: const [
-                  ProductSimpleView(
-                    linkList: [
-                      "https://http2.mlstatic.com/D_NQ_NP_682844-MLA47517377636_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_786969-MLA47560237400_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp"
-                    ],
-                    productDetail: "Sponje Bob T-shirt",
-                    price: 10.99,
-                    dropDown: true,
-                    dropDownValues: ["S", "M", "L", "XL"],
-                  ),
-                  ProductSimpleView(
-                    linkList: [
-                      "https://http2.mlstatic.com/D_NQ_NP_682844-MLA47517377636_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_786969-MLA47560237400_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp",
-                      "https://http2.mlstatic.com/D_NQ_NP_918559-MLA45861441541_052021-O.webp"
-                    ],
-                    productDetail: "Sponje Bob T-shirt",
-                    price: 10.99,
-                    dropDown: true,
-                    dropDownValues: ["S", "M", "L", "XL"],
-                  ),
-                ]),
+              if (snapshot.hasData && !snapshot.data!.exists) {
+                return const Text("Document does not exist");
+              }
+
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> data =
+                    snapshot.data!.data() as Map<String, dynamic>;
+                return Text("Name: ${data['productDetail']} ${data['price']}");
+              }
+
+              return const Text("loading");
+            },
           ),
-        ),
+          Center(
+            child: SizedBox(
+              width: widthSize,
+              // height: heightSize,
+              //constraints: BoxConstraints(minWidth: minWidth, minHeight: minHeight),
+
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.only(top: 20),
+                physics: const BouncingScrollPhysics(),
+                child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 2,
+                    mainAxisSpacing: 2,
+                    childAspectRatio: 0.8,
+                    shrinkWrap: true,
+                    children: const [
+                      ProductSimpleView(
+                        linkList: [
+                          "https://http2.mlstatic.com/D_NQ_NP_682844-MLA47517377636_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_786969-MLA47560237400_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp"
+                        ],
+                        productDetail: "Sponje Bob T-shirt",
+                        price: 10.99,
+                        dropDown: true,
+                        dropDownValues: ["S", "M", "L", "XL"],
+                      ),
+                      ProductSimpleView(
+                        linkList: [
+                          "https://http2.mlstatic.com/D_NQ_NP_682844-MLA47517377636_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_786969-MLA47560237400_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp",
+                          "https://http2.mlstatic.com/D_NQ_NP_918559-MLA45861441541_052021-O.webp"
+                        ],
+                        productDetail: "Sponje Bob T-shirt",
+                        price: 10.99,
+                        dropDown: true,
+                        dropDownValues: ["S", "M", "L", "XL"],
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
