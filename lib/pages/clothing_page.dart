7f912afe1a597929e1ce69a09baeb404b8542056
Le127 <nereocarbonara@gmail.com>
2021-11-03 02:04:02 -0300

    First Firestore connection OK

--- lib/pages/clothing_page.dart
+++ lib/pages/clothing_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecomm_app/db/helpers/clothing_helpers.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ecomm_app/widget/product_simple_view.dart';
@@ -8,7 +9,7 @@ class ClothingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-  
+
     double widthSize = 350;
 
     if (size.width >= 1091 && size.height >= 624) {
@@ -16,9 +17,27 @@ class ClothingPage extends StatelessWidget {
     }
 
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        final clothes = Clothings(
+            productDetail: "Sponje Bob T-shirt",
+            linkList: [
+              "https://http2.mlstatic.com/D_NQ_NP_682844-MLA47517377636_092021-O.webp",
+              "https://http2.mlstatic.com/D_NQ_NP_786969-MLA47560237400_092021-O.webp",
+              "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp",
+              "https://http2.mlstatic.com/D_NQ_NP_796991-MLA47480511712_092021-O.webp"
+            ],
+            price: 25,
+            dropDown: true,
+            dropDownValues: ["S", "M", "L", "XL"]);
+
+        clothes.addClothes(clothes);
+      }),
       appBar: AppBar(
-        iconTheme:const IconThemeData(color: Colors.black),
-        title: const Text("Clothing Page", style: TextStyle(color: Colors.black),),
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: const Text(
+          "Clothing Page",
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: Colors.yellow.shade300.withOpacity(0.9),
       ),
       body: Center(
